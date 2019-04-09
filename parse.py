703cc7284de3b0532fe1747628bda9fabcd471e7
Harin Sanghirun <harin.sanghirun@gmail.com>
2019-04-08 22:58:33 -0400

    fix missing duration info

--- parse.py
+++ parse.py
@@ -1,6 +1,12 @@
 from bs4 import BeautifulSoup
 import os
 from pathlib import Path
+import pickle
+import json
+import pprint
+import re
+
+pp = pprint.PrettyPrinter(indent=2)
 
 def record_selection(sel, record):
     """Take a selection of key value list and add them to a dict
@@ -14,7 +20,7 @@ def record_selection(sel, record):
 
     record[sel[0].text.strip()] = sel[1].text.strip()
 
-def parse_experienes(soup):
+def parse_experiences(soup):
     experiences = []
     for content in soup.select('#experience-section .pv-profile-section__card-item-v2'):
         multi_role_company = content.select_one('.pv-entity__company-details')
@@ -30,9 +36,13 @@ def parse_experienes(soup):
                 experiences.append(exp)
         else:
             experience = {}
-            experience['role'] = content.select_one('.pv-entity__summary-info h3').text.strip()
+            context = content.select_one('.pv-entity__summary-info')
+            experience['role'] = context.select_one('h3').text.strip()
             experience['company'] = content.select_one('.pv-entity__secondary-title').text.strip()
+            for item in context.select('h4'):
+                record_selection(item.select('span'), experience)
             experiences.append(experience)
+            
     return experiences
 
 def parse_educations(soup):
@@ -61,15 +71,31 @@ def parse_linkedin_profile(html):
         except AttributeError:
             result[key] = None
 
-    result['experiences'] = parse_experienes(soup)
+    result['experiences'] = parse_experiences(soup)
     result['educations'] = parse_educations(soup)
     return result
 
 if __name__ == '__main__':
     folder = Path('./pages')
+    results = []
+    i = 0
     for fname in os.listdir(folder):
+        if not re.match('.+.html$', fname):
+            continue
+        i+=1
+        # if i > 3:
+            # break
         print('parsing', fname)
         with open(folder/fname, 'r') as f:
             html = f.read()
             result = parse_linkedin_profile(html)
-            print(result)
+            if result['name'] != None:
+                results.append(result)
+        
+    # pp.pprint(results)
+
+    with open('./profiles.p', 'wb') as f:
+        pickle.dump(results, f) 
+        
+    with open('./profiles2.json', 'w') as f:
+        json.dump(results, f)
